484864f942b40dff69b381d03778b212a27b864f
vakhula <andrii.vakhula@gmail.com>
2023-09-14 09:15:17 +0300

    initial_improvments

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,28 @@ import { observer } from 'mobx-react-lite';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import classNames from 'classnames';
+import { RadioSwitch } from './RadioSwitch'
 
 // ---
 
 const genUUID = (): string => crypto.randomUUID();
+
+function arrayMove(arr, oldIndex, newIndex) {
+  if (newIndex >= arr.length) {
+      let k = newIndex - arr.length + 1;
+      while (k--) {
+          arr.push(undefined);
+      }
+  }
+  arr.splice(newIndex, 0, arr.splice(oldIndex, 1)[0]);
+  return arr;
+};
+
+const arrayInsert = (arr, index, newItem) => [
+  ...arr.slice(0, index),
+  newItem,
+  ...arr.slice(index)
+]
 
 // ---
 
@@ -30,12 +48,21 @@ class RowElement implements Element {
   }
 }
 
+interface TextSettings {
+  size: string
+  weight: string
+}
+
+const defaultTextSettings = { size: 'font-size-md', weight: 'font-weight-md' }
+
 class TextElement implements Element {
   type: ElementType = 'text';
   text: string;
+  settings: TextSettings
 
-  constructor(options: { text: string }) {
+  constructor(options: { text: string, settings?: TextSettings }) {
     this.text = options.text;
+    this.settings = options.settings || defaultTextSettings;
   }
 }
 
@@ -45,6 +72,7 @@ class ElementsStore {
   }
 
   elements: Element[] = [];
+  selectedElement: Element | TextElement | null = null
 
   register(elements: Element[]) {
     this.elements = elements;
@@ -171,10 +199,13 @@ class TextNode implements Node {
   text: string;
   children: Record<string, Node> = {};
   parent: Node;
+  settings: TextSettings
 
   constructor(node: TextNode | TextElement, parentNode: Node) {
+    makeAutoObservable(this)
     this.text = node.text;
     this.parent = parentNode;
+    this.settings = node?.settings || defaultTextSettings;
   }
 }
 
@@ -207,6 +238,7 @@ class CanvasStore {
   nodes = new RootNode();
 
   selectedNode: Node | null = null;
+  selectedRowNode: Node | null = null;
 
   saveAdapters: Adapter[] = [];
   loadAdapters: Adapter[] = [];
@@ -222,11 +254,38 @@ class CanvasStore {
   setSelectedNode(node: Node) {
     this.selectedNode = node;
   }
+  setSelectedRowNode(node: Node) {
+    this.selectedRowNode = node;
+  }
+  addRowNode(elem: RowElement, index) {
+    const arr = Object.values(this.nodes.children)
+    const newArr = arrayInsert(arr, index, new RowNode({ cols: elem.cols, parentNode: this.nodes }))
+    this.nodes.children = Object.values(newArr)
+      .map((node) => new RowNode({ node, parentNode: this.nodes }))
+      .reduce((acc, el) => {
+        acc[el.id] = el;
+        return acc;
+      }, {});
+  }
 
-  addRowNode(elem: RowElement) {
-    const rowNode = new RowNode({ cols: elem.cols, parentNode: this.nodes });
+  moveRowNode(elem: RowElement, targetIndex: number) {
+    const arr = Object.values(this.nodes.children)
+    const oldIndex = arr.findIndex((i) => i.id === elem.id)
 
-    this.nodes.children[rowNode.id] = rowNode;
+    let newArr
+    if (oldIndex > targetIndex) {
+      newArr = arrayMove(arr, oldIndex, targetIndex)
+    } else {
+      newArr = arrayMove(arr, oldIndex, targetIndex - 1)
+    }
+
+    this.nodes.children = Object.values(newArr)
+      .map((node) => new RowNode({ node, parentNode: this.nodes }))
+      .reduce((acc, el) => {
+        acc[el.id] = el;
+        return acc;
+      }, {});
+
   }
 
   removeNode(node: Node) {
@@ -250,6 +309,12 @@ class CanvasStore {
       const textNode = new TextNode(node, parenNode);
 
       parenNode.children[textNode.id] = textNode;
+    }
+  }
+
+  updateNodeSettings (key, value) {
+    if (this.selectedNode) {
+      this.selectedNode.settings[key] =value
     }
   }
 
@@ -325,8 +390,11 @@ const Builder: React.FC<Props> = observer(({ children }) => {
   return <div className="builder">{children}</div>;
 });
 
-const TextNodeRenderer: React.FC<{ node: TextNode; canvasStore: CanvasStore }> = observer(
-  ({ node, canvasStore }) => {
+const TextNodeRenderer: React.FC<{
+  node: TextNode;
+  canvasStore: CanvasStore,
+  elementsStore:ElementsStore,
+}> = observer(({ node, canvasStore }) => {
     const [{ isDragging }, drag] = useDrag(() => ({
       type: 'text',
       item: node,
@@ -337,26 +405,31 @@ const TextNodeRenderer: React.FC<{ node: TextNode; canvasStore: CanvasStore }> =
 
     return (
       <div
+        onClick={(e) => {
+          canvasStore.setSelectedNode(node)
+        }}
         className={node.className}
         ref={drag}
         style={{
-          background: isDragging ? 0.5 : 1,
+          background: isDragging ? 'greenyellow' : '',
         }}
       >
-        <div>{node.text}</div>
+        <div className={`text-value ${Object.values(node.settings).join(' ')}`}>{node.text}</div>
         <NodeControls onRemove={() => canvasStore.removeNode(node)} />
       </div>
     );
   }
 );
 
-const ColNodeRenderer: React.FC<{ node: ColNode; canvasStore: CanvasStore }> = observer(
+const ColNodeRenderer: React.FC<{ node: ColNode; canvasStore: CanvasStore, elementsStore: ElementsStore }> = observer(
   ({ node: col, canvasStore }) => {
     const [{ isOver }, drop] = useDrop(
       () => ({
         accept: 'text',
         drop: (item: Node | Element) => {
-          canvasStore.addNode(item, col);
+          if (Object.values(col.children).length === 0) {
+            canvasStore.addNode(item, col);
+          }
         },
         collect: (monitor) => ({
           isOver: !!monitor.isOver(),
@@ -367,15 +440,21 @@ const ColNodeRenderer: React.FC<{ node: ColNode; canvasStore: CanvasStore }> = o
 
     return (
       <div
+        // onClick={(e) => {
+        //   console.log(e.target.className)
+        //   if (e.target.className === 'col-node') {
+        //     canvasStore.setSelectedNode(null)
+        //   }
+        // }}
         className={classNames(col.className, {
           '--is-over': isOver,
         })}
         ref={drop}
       >
-        {Object.values(col.children).map((node) => {
+        {Object.values(col.children).map((node, index) => {
           // TODO: strategy
           if (node instanceof TextNode) {
-            return <TextNodeRenderer key={node.id} node={node} canvasStore={canvasStore} />;
+            return <TextNodeRenderer index={index} key={node.id} node={node} elementsStore={elementsStore} canvasStore={canvasStore} />;
           }
         })}
       </div>
@@ -386,15 +465,37 @@ const ColNodeRenderer: React.FC<{ node: ColNode; canvasStore: CanvasStore }> = o
 const NodeControls: React.FC<{ onRemove: () => void }> = observer(({ onRemove }) => {
   return (
     <div className="controls">
-      <div onClick={onRemove}>ⅹ</div>
+      <div onClick={onRemove}>❌</div>
     </div>
   );
 });
+const DropArea = ({index, className}: {index: number, className?: string}) => {
+  const [{ isOver }, drop] = useDrop(() => ({
+      accept: 'row',
+      drop: (item: RowElement) => {
+
+        if (item.id) {
+          canvasStore.moveRowNode(item, index);
+        } else {
+          canvasStore.addRowNode(item, index);
+        }
+      },
+
+      collect: (monitor) => ({
+        isOver: !!monitor.isOver(),
+      }),
+    }))
+  return (
+    <div className={`drop-area ${isOver ? 'drop-area--is-over' : '' } ${className}`} ref={drop}/>
+  )
+}
 
 const RowNodeRenderer: React.FC<{
   node: RowNode;
   canvasStore: CanvasStore;
-}> = observer(({ node, canvasStore }) => {
+  elementsStore: ElementsStore;
+  index: number
+}> = observer(({ node, canvasStore, elementsStore, index }) => {
   const [{ isDragging }, drag] = useDrag(() => ({
     type: 'row',
     item: node,
@@ -406,35 +507,30 @@ const RowNodeRenderer: React.FC<{
   return (
     <div
       className={node.className}
-      onClick={() => canvasStore.setSelectedNode(node)}
+      onClick={(e) => {
+        canvasStore.setSelectedRowNode(node)
+
+        if (e.target.className === 'row-node') {
+          canvasStore.setSelectedNode(null)
+        }
+      }}
       ref={drag}
       style={{
         opacity: isDragging ? 0.5 : 1,
       }}
     >
       <NodeControls onRemove={() => canvasStore.removeNode(node)} />
-
+      <DropArea key={index} index={index} />
       {Object.values(node.children).map((node) => (
-        <ColNodeRenderer key={node.id} node={node} canvasStore={canvasStore} />
+        <ColNodeRenderer key={node.id} node={node} canvasStore={canvasStore} elementsStore={elementsStore} />
       ))}
     </div>
   );
 });
 
-const Canvas: React.FC<{ canvasStore: CanvasStore }> = observer(({ canvasStore }) => {
-  const [{ isOver }, drop] = useDrop(
-    () => ({
-      accept: 'row',
-      drop: (item: RowElement) => {
-        canvasStore.addRowNode(item);
-      },
-      collect: (monitor) => ({
-        isOver: !!monitor.isOver(),
-      }),
-    })
-    // [x, y] deps!
-  );
+const Canvas: React.FC<{ canvasStore: CanvasStore, elementsStore: ElementsStore }> = observer(({ canvasStore, elementsStore }) => {
   const rootEl = canvasStore.nodes;
+  const lastIndex = Object.values(rootEl.children).length
 
   return (
     <div className="builder__canvas">
@@ -447,18 +543,21 @@ const Canvas: React.FC<{ canvasStore: CanvasStore }> = observer(({ canvasStore }
           ❌
         </div>
       </div>
-      <div className={classNames('root', { 'root--is-over': isOver })} ref={drop}>
-        <div>root elem</div>
-
-        {Object.values(rootEl.children).map((node) => {
-          return <RowNodeRenderer key={node.id} node={node} canvasStore={canvasStore} />;
+      <div className={classNames('root')}>
+        {Object.values(rootEl.children).map((node, index) => {
+          return <RowNodeRenderer key={`${index}_${node.id}`} index={index} node={node} canvasStore={canvasStore} elementsStore={elementsStore} />
         })}
+        <DropArea
+          key={lastIndex}
+          index={lastIndex}
+          className={`root_drop ${lastIndex === 0 ? 'empty' : ''}`}
+        />
       </div>
     </div>
   );
 });
 
-const ElementRenderer: React.FC<{ element: Element }> = observer(({ element }) => {
+const ElementRenderer: React.FC<{ element: Element, elementsStore: ElementsStore }> = observer(({ element }) => {
   const [{ isDragging }, drag] = useDrag(() => ({
     type: element.type,
     item: element,
@@ -496,33 +595,66 @@ const Elements: React.FC<{ elementsStore: ElementsStore }> = observer(({ element
       <div>Elements</div>
       <div>
         {elementsStore.elements.map((element, index) => {
-          return <ElementRenderer key={index} element={element} />;
+          return <ElementRenderer key={index} elementsStore={elementsStore} element={element} />;
         })}
       </div>
     </div>
   );
 });
 
-// const ElementSettings: React.FC = () => {
-//   return <div className="builder__element-settings">ElementSettings</div>;
-// };
-
-// const UISettings: React.FC = () => {
-//   return <div>UISettings</div>;
-// };
-
-// ---
+const ElementSettings: React.FC<{canvasStore: CanvasStore}> = observer(({ canvasStore }) => {
+  return (
+    <div className="builder__element-settings">
+      {canvasStore?.selectedRowNode &&
+        <>
+          <div>Block Settings:</div>
+          {canvasStore?.selectedRowNode?.text}-{Object.values(toJS(canvasStore?.selectedRowNode?.children)|| {}).length}
+        </>
+      }
+      <br />
+      {canvasStore.selectedNode?.settings &&
+        <>
+          <div>Node Settings:</div>
+          {Object.keys(canvasStore.selectedNode?.settings).map((settingName) => (
+            <RadioSwitch
+            label={`${settingName}: `}
+            options={[{
+              label: 'Sm',
+              value: `font-${settingName}-sm`
+            },
+            {
+              label: 'Md',
+              value: `font-${settingName}-md`
+            },
+            {
+              label: 'Lg',
+              value: `font-${settingName}-lg`
+            }]}
+            value={canvasStore?.selectedNode?.settings[settingName]}
+            onChange={(val) => {
+              canvasStore.updateNodeSettings(settingName,  val)
+            }}
+          />
+          ))}
+        </>
+      }
+    </div>
+  );
+});
 
 export default function App() {
   return (
     <div className="App">
       <Builder>
         <DndProvider backend={HTML5Backend}>
-          {/* <ElementSettings /> */}
+          <div>
+            <ElementSettings canvasStore={canvasStore} />
+          </div>
           <Elements elementsStore={elementsStore} />
-          <Canvas canvasStore={canvasStore} />
+          <Canvas canvasStore={canvasStore} elementsStore={elementsStore} />
         </DndProvider>
       </Builder>
     </div>
   );
 }
+
